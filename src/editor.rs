c4d02ca947176108b65aa8847f6a841d0a357241
Rachid_Boufous <rachidboufous32@gmail.com>
2024-01-21 02:53:54 +0100

    organizing the code - moving it from main to editor.rs

---
+++ src/editor.rs
@@ -0,0 +1,49 @@
+use std::io::{self, stdout};
+use termion::event::Key; // we import the Key enum
+use termion::input::TermRead; // we import the TermRead trait
+use termion::raw::IntoRawMode; //
+
+pub struct Editor {
+    // a struct is a collection of variables, functions, which are grouped together to form an unity
+    // pub: means that we can access this struct from outside the file
+}
+
+impl Editor {
+    pub fn run(&self) {
+        let _stdout = stdout().into_raw_mode().unwrap(); // read data from standard input (the keyboard)
+
+        for key in io::stdin().keys() {
+            // now we are reading the keys pressed by the user
+            match key {
+                // we match the key pressed by the user
+                Ok(key) => match key {
+                    // we match the key pressed by the user
+                    Key::Char(c) => {
+                        // if the user presses a character key, we print the character
+                        if c.is_control() {
+                            // if the character is a control character, we print its code
+                            println!("{:?}\r", c as u8); // the u8 cast converts the character to its byte value
+                        } else {
+                            println!("{:?} ({})\r", c as u8, c); // otherwise, we print the character and its code
+                        }
+                    }
+
+                    Key::Ctrl('q') => break, // if the user presses ctrl + q , we exit the program
+                    _ => println!("{:?}\r", key), // otherwise, we do nothing
+                },
+
+                Err(err) => die(err),
+            }
+        }
+    }
+}
+
+
+/// Handles an error by panicking.
+///
+/// # Arguments
+///
+/// * `e` - The error to handle.
+fn die(e: std::io::Error) {
+    panic!("{}", e); // a macro that crashes the program and prints the error
+}
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,8 @@
-use std::io::{self, stdout, Read};
-use termion::raw::IntoRawMode;
+mod editor; // here we are importing the editor module
 
-/// Converts a character to its corresponding control byte.
-///
-/// # Arguments
-///
-/// * `c` - The character to convert.
-///
-/// # Returns
-///
-/// The control byte of the character.
-fn to_ctrl_byte(c: char) -> u8 {
-    let byte = c as u8;
-    byte & 0b0001_1111
-}
-
-/// Handles an error by panicking.
-///
-/// # Arguments
-///
-/// * `e` - The error to handle.
-fn die(e: std::io::Error) {
-    panic!("{}", e); // a macro that crashes the program and prints the error
-}
+use editor::Editor; // here we are importing the Editor struct from the editor module
 
 fn main() {
-    let _stdout = stdout().into_raw_mode().unwrap(); // read data from standard input (the keyboard)
-
-    for b in io::stdin().bytes() { // we loop through each byte of the input
-        match b {  // match is like a switch statement
-            Ok(b) => { // if the byte is valid (not an error)
-                let c = b as char; // convert the byte to a character
-
-                if c.is_control() { // we check if the character is a control character
-                    println!("{:?} \r", b); // if it is, we print the byte
-                } else {
-                    println!("{:?} ({})\r", b, c); // otherwise, we print the byte and the character
-                }
-
-
-                if b == to_ctrl_byte('q') { // if the byte is the control byte of the letter q
-                        break;
-                }
-
-            }
-            Err(e) => die(e), // if the byte is invalid, crash the program
-        };
-    }
+    let editor = Editor {}; // we create an instance of the Editor struct
+    editor.run(); // we call the run method of the Editor struct
 }
