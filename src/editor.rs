52505f92cb8257f105f20eee149c514fc38b05e0
Rachid_Boufous <rachidboufous32@gmail.com>
2024-02-03 23:28:35 +0100

    adding support for opening files with terminal

--- src/editor.rs
+++ src/editor.rs
@@ -50,7 +50,7 @@ impl Editor {
             should_quit:false, // we are initializing the struct with the should_quit variable set to false
             terminal: Terminal::default().expect("Failed to initialize terminal"), // we are initializing the terminal
             cursor_position: Position {x: 0, y:0},
-            document: Document::open(),
+            document: Document::default(),
         }
     }
 
@@ -147,7 +147,7 @@ impl Editor {
             if let Some(row) = self.document.row(terminal_row as usize) {
                 self.draw_row(row);
             }
-            else if terminal_row == height / 3 {
+            else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
             }
             else {
