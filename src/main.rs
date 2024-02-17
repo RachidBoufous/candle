ca68996140f494b873ee68739de5185cf6dc6eb1
Rachid_Boufous <rachidboufous32@gmail.com>
2024-02-17 22:34:06 +0100

    support for files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-#![warn(clippy::all, clippy::pedantic)]
+// #![warn(clippy::all, clippy::pedantic)]
 
 mod editor; // here we are importing the editor module
 mod terminal; // here we are importing the terminal module
